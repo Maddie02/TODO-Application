8cbdf2f155c2fda720ff7561f186e32799ad1aa0
Madlen <madlensarkisian@gmail.com>
2020-05-21 23:42:27 +0300

    [Update] Add get route for lists

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const mongoose = require('./database/mongoose');
+const List = require('./database/models/list');
+const Task = require('./database/models/task');
 
 const app = express();
+
+app.use(express.json);
+
+app.get('/lists', (req, res) => {
+    Task.find({})
+        .then(lists => res.send(lists))
+        .catch((error) => console.log(error));
+})
+
 
 app.listen(3000, () => console.log('Server connected on port 3000!'));
