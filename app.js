1346366c5755f8658d129174ac7010c825ec9bdd
Madlen <madlensarkisian@gmail.com>
2020-05-24 22:44:25 +0300

    [Update] Added routes for tasks

--- app.js
+++ app.js
@@ -31,8 +31,52 @@ app.patch('/lists/:listId', async (req, res) => {
 })
 
 app.delete('/lists/:listId', async (req, res) => {
-    await List.findByIdAndDelete(req.params.listId)
-    res.send('Succesfully deleted!');
+
+    const deleteTasks = (list) => {
+        Task.deleteMany({_listId: list._id})
+            .then(() => res.send('Succesfully deleted list and tasks!'))
+            .catch((error) => console.log(error));
+    }
+
+    List.findByIdAndDelete(req.params.listId)
+        .then((list) => deleteTasks(list))
+        .catch((error) => console.log(error));
+})
+
+app.get('/lists/:listId/tasks', async (req, res) => {
+    let tasks = await Task.find({ _listId: req.params.listId });
+    res.send(tasks);
+})
+
+app.post('/lists/:listId/tasks', async (req, res) => {
+    let newTask = new Task({
+        title: req.body.title,
+        _listId: req.params.listId
+    });
+
+    try {
+        newTask.save();
+        res.send(newTask);
+    }
+    catch (error) {
+        console.log(error);
+    }
+    
+})
+
+app.get('/lists/:listId/tasks/:taskId', async (req, res) => {
+    let task = await Task.find({ _id: req.params.taskId });
+    res.send(task);
+})
+
+app.patch('/lists/:listId/tasks/:taskId', async (req, res) => {
+    let updatedTask = await Task.findByIdAndUpdate(req.params.taskId, { $set: req.body })
+    res.send(updatedTask);
+})
+
+app.delete('/lists/:listId/tasks/:taskId', async (req, res) => {
+    let taskToBeDeleted = await Task.findByIdAndDelete(req.params.taskId);
+    res.send("Succesfully deleted!");
 })
 
 app.listen(3000, () => console.log('Server connected on port 3000!'));
