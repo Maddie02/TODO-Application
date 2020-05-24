7c013eafba87314d9dfe3ce7ac1ba62f0bdd2c59
Madlen <madlensarkisian@gmail.com>
2020-05-24 22:05:20 +0300

    [Update] Added routes for GET, UPDATE, DELETE lists

--- app.js
+++ app.js
@@ -4,14 +4,35 @@ const List = require('./database/models/list');
 const Task = require('./database/models/task');
 
 const app = express();
-
-app.use(express.json);
+app.use(express.json());
 
 app.get('/lists', (req, res) => {
-    Task.find({})
-        .then(lists => res.send(lists))
+    List.find({})
+    .then(lists => res.send(lists))
+    .catch((error) => console.log(error));
+})
+
+app.post('/lists', (req, res) => {
+    let list = new List(req.body);
+
+    list.save()
+        .then((list) => res.send(list))
         .catch((error) => console.log(error));
 })
 
+app.get('/lists/:listId', async (req, res) => {
+    let foundList = await List.find({ _id: req.params.listId });
+    res.send(foundList);
+})
+
+app.patch('/lists/:listId', async (req, res) => {
+    let updatedList = await List.findByIdAndUpdate(req.params.listId, { $set: req.body })
+    res.send(updatedList);
+})
+
+app.delete('/lists/:listId', async (req, res) => {
+    await List.findByIdAndDelete(req.params.listId)
+    res.send('Succesfully deleted!');
+})
 
 app.listen(3000, () => console.log('Server connected on port 3000!'));
--- database/mongoose.js
+++ database/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://127.0.0.1:27017/todolist', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://127.0.0.1:27017/todolist', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
     .then(() => console.log('Database connected!'))
     .catch((error) => console.log(error));
 
